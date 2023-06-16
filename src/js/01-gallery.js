a7d2f7e3013300af818399344d5ebedf3835080b
Rummut <kzantip@gmail.com>
2023-06-16 23:27:09 +0300

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+
+// Change code below this line
+const galleryEl = document.querySelector(".gallery");
+
+const markUp = galleryItems
+  .map(
+      ({ preview, original, description }) => {
+          const galleryItems = document.createElement('li')
+          galleryItems.classList.add("gallery__item")
+          galleryEl.append(galleryItems)
+
+          const galleryLink = document.createElement('a')
+          galleryLink.classList.add("gallery__link")
+          galleryLink.href = `${original}`
+          galleryItems.append(galleryLink)
+
+          const image = document.createElement('img')
+          image.classList.add("gallery__image")
+          image.src = `${preview}`
+          image.alt = `${description}`
+          galleryLink.append(image)
+      }
+  ).join("");
+  
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250});
+gallery.on('show.simplelightbox')
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+player.on('eventName', function(data) {
+    // data is an object containing properties specific to that event
+});
